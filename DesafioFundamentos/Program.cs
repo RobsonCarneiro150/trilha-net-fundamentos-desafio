1167d00dc14117a5ed439113ee4e68d66b9c393f
Robson Carneiro <carneiropp150@gmail.com>
2023-03-27 21:20:28 -0300

    Desafio Concluido!

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,4 +1,5 @@
-﻿using DesafioFundamentos.Models;
+﻿//Concluido!
+using DesafioFundamentos.Models;
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -9,7 +10,6 @@ decimal precoPorHora = 0;
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
-
 Console.WriteLine("Agora digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
